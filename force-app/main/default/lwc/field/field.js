695536f3817bfa547223dca6ca711ac7125e62d7
kishan khunt <kkhunt@ardira.com>
2020-03-23 12:17:18 +0530

    Field named component that contains the AccName and Phone

--- force-app/main/default/lwc/field/field.js
+++ force-app/main/default/lwc/field/field.js
@@ -1,26 +1,46 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, wire } from "lwc";
 import { createRecord } from "lightning/uiRecordApi";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import getAcccountList from "@salesforce/apex/AccountRecord.getAccountList";
 
 import ACCOUNT_OBJECT from "@salesforce/schema/Account";
 import Name_FIELD from "@salesforce/schema/Account.Name";
 import PHONE_FIELD from "@salesforce/schema/Account.Phone";
 
 export default class Fields extends LightningElement {
-  accountId;
   name = "";
   phone = "";
 
   changehandlername(event) {
-    this.accountId = undefined;
     this.name = event.target.value;
   }
 
   changehandlerphone(event) {
-    this.accountId = undefined;
     this.phone = event.target.value;
   }
 
+  //console.log("start");
+  // var phoneno = /^\d{10}$/;
+  // if (phone.value.match(phoneno)) {
+  //   return true;
+  //   console.log("success");
+  // } else {
+  //   alert("not valid phone number");
+  //   return false;
+  //   console.log("fail");
+  // }
+
+  cancelhandler() {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: "cancel",
+        message: "menu has Cancelled",
+        variant: "cancel"
+      })
+    );
+  }
+
+  // phonenumber() {}
   //const field = event.target.name;
   // if (field === "firstname") {
   //   this.firstname = event.target.value;
@@ -33,15 +53,12 @@ export default class Fields extends LightningElement {
     fields[Name_FIELD.fieldApiName] = this.name;
     fields[PHONE_FIELD.fieldApiName] = this.phone;
     const recordInput = { apiName: ACCOUNT_OBJECT.objectApiName, fields };
-
-    console.log(recordInput);
+    //console.log(recordInput);
 
     createRecord(recordInput)
       .then(account => {
-        this.accountId = account.Id;
-
-        console.log("sucess");
-
+        eval("$A.get('e.force:refreshView').fire();");
+        console.log("success");
         this.dispatchEvent(
           new ShowToastEvent({
             title: "Success",
@@ -52,13 +69,27 @@ export default class Fields extends LightningElement {
       })
       .catch(error => {
         console.log("fail");
+
         this.dispatchEvent(
           new ShowToastEvent({
-            title: "Error",
-            message: "Error has occored",
+            title: "mobile number restrict to 10 digit",
+            message: "set 10 digit mobile number",
             variant: "error"
           })
         );
       });
   }
 }
+
+// .catch(error => {
+//   //   console.log("fail");
+//   this.dispatchEvent(
+//     new ShowToastEvent({
+//       title: "Error",
+//       message: "Error has occored",
+//       variant: "error"
+//     })
+//   );
+// });
+// }
+// }
